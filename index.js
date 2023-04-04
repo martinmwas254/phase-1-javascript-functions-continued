511c5c799ea3aed7b757fa7ebd2943ad9e7f2d47
martinmwas254 <martinmwas264@gmail.com>
2023-04-04 15:24:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
-}
-
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
-}
-
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+function saturdayFun(activity) {
+  if (activity === undefined) {
+    activity = "roller-skate";
   }
+  return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+
+function mondayWork(activity) {
+  if (activity === undefined) {
+    activity = "go to the office";
+  }
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(highlight = "*") {
+  return function (adjective) {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
 }
